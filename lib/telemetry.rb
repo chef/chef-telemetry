d5beed5823fa68203aaac0bab10e1c369e3433a6
Thom May <thom@chef.io>
2017-09-18 18:46:37 +0100

    Implement the API
    Signed-off-by: Thom May <thom@chef.io>

--- lib/telemetry.rb
+++ lib/telemetry.rb
@@ -5,5 +5,33 @@ require "telemetry/session"
 require "telemetry/version"
 
 class Telemetry
-  # Your code goes here...
+  attr_accessor :product, :origin
+  def initialize(product: nil, origin: "command-line")
+    @product = product
+    @origin = origin
+  end
+
+  def deliver(data = {})
+    unless opt_out?
+      payload = event.prepare(data)
+      client.async.fire(payload)
+    end
+  end
+
+  def event
+    @event ||= Event.new(product, session, origin)
+  end
+
+  def session
+    @session ||= Session.new
+  end
+
+  def opt_out?
+    @opt_out ||= Decision.opt_out?
+  end
+
+  def client
+    endpoint = ENV.fetch("CHEF_TELEMETRY_ENDPOINT", Client::TELEMETRY_ENDPOINT)
+    @client ||= Client.new(endpoint)
+  end
 end
--- spec/telemetry_spec.rb
+++ spec/telemetry_spec.rb
@@ -1,8 +1,52 @@
 require "spec_helper"
 
 RSpec.describe Telemetry do
-  it "has a version number" do
-    expect(Telemetry::VERSION).not_to be nil
+  let(:payload) do
+    {
+      properties: {},
+    }
+  end
+  let(:env) { {} }
+
+  describe "#client" do
+    let(:endpoint) { "https:://my.telemetry.endpoint" }
+    before do
+      stub_const("ENV", env)
+    end
+
+    describe "creates a new client" do
+      it "with the default endpoint" do
+        expect(Telemetry::Client).to receive(:new).with(Telemetry::Client::TELEMETRY_ENDPOINT)
+        subject.client
+      end
+
+      it "using an environment key" do
+        env["CHEF_TELEMETRY_OPT_OUT"] = endpoint
+        expect(Telemetry::Client).to receive(:new).with(endpoint)
+        subject.client
+      end
+    end
   end
 
+  describe "opted in" do
+    before do
+      expect(Telemetry::Decision).to receive(:opt_out?).and_return(false)
+    end
+
+    it "sends an event" do
+      expect(subject.event).to receive(:prepare).with(payload).and_call_original
+      expect(subject.deliver(payload)).to be_truthy
+    end
+  end
+
+  describe "opted out" do
+    before do
+      expect(Telemetry::Decision).to receive(:opt_out?).and_return(true)
+    end
+
+    it "doesn't send an event" do
+      expect(subject).to_not receive(:event)
+      subject.deliver(payload)
+    end
+  end
 end
