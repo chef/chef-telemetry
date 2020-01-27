f8ccdf1d4f8468642009baddbaebef00701b6ff7
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-01-27 14:21:28 -0500

    Lowercase and dashify the ENV values to match license-acceptance
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/chef/telemetry/decision/environment.rb
+++ lib/chef/telemetry/decision/environment.rb
@@ -12,25 +12,25 @@ class Chef
         end
 
         def opt_in?
-          env_seek("OPT_IN")
+          env_seek("opt-in")
         end
 
         def opt_out?
-          env_seek("OPT_OUT")
+          env_seek("opt-out")
         end
 
         def opt_in_no_persist?
-          env_seek("OPT_IN_NO_PERSIST")
+          env_seek("opt-in-no-persist")
         end
 
         def opt_out_no_persist?
-          env_seek("OPT_OUT_NO_PERSIST")
+          env_seek("opt-out-no-persist")
         end
 
         private
 
         def env_seek(what)
-          if env["CHEF_TELEMETRY"] && env["CHEF_TELEMETRY"].upcase == what
+          if env["CHEF_TELEMETRY"] && env["CHEF_TELEMETRY"].downcase == what
             return true
           end
           false
